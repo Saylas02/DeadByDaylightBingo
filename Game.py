5ccf58435d96f1464ec26cc32130fe6a8f99d384
hermessil <Silas.Hermes02@gmail.com>
2025-02-09 18:35:45 +0100

    setup classes

---
+++ Game.py
@@ -0,0 +1,4 @@
+class Game:
+    def __init__(self, player: [], boards: []):
+        self.player: [] = player
+        self.boards: [] = boards
---
+++ Player.py
@@ -0,0 +1,5 @@
+class Player:
+    def __init__(self, name: str, score: int, deck: []):
+        self.name = name
+        self.score = score
+        self.deck = deck
