f4564efc3662e2449afe299ee8364e1c4796f66c
hermessil <Silas.Hermes02@gmail.com>
2025-02-09 18:53:45 +0100

    setup classes for Player.py

--- Player.py
+++ Player.py
@@ -1,5 +1,20 @@
 class Player:
-    def __init__(self, name: str, score: int, deck: []):
-        self.name = name
-        self.score = score
-        self.deck = deck
+    def __init__(self, name: str, deck: [], balance: int):
+        self.name: str = name
+        self.deck: [] = deck
+        balance: int = balance
+
+    def set_name(self , name: str):
+        self.name: str = name
+
+    def get_name(self):
+        return self.name
+
+    def set_deck(self, deck: []):
+        self.deck: [] = deck
+
+    def get_deck(self):
+        return self.deck
+
+    def set_balance(self, balance: int):
+        self.balance: int = balance
