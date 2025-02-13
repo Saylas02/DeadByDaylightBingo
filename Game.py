38d3ff3bd75b222c9d89be949ee09a2562976dde
hermessil <Silas.Hermes02@gmail.com>
2025-02-13 18:56:34 +0100

    setup setter and getter for Game.py

--- Game.py
+++ Game.py
@@ -1,4 +1,16 @@
 class Game:
-    def __init__(self, player: [], boards: []):
+    def __init__(self, player: [], board: []):
         self.player: [] = player
-        self.boards: [] = boards
+        self.board: [] = board
+
+    def set_player(self, player: []):
+        self.player = player
+
+    def get_player(self) -> []:
+        return self.player
+
+    def set_board(self, board: []):
+        self.board = board
+
+    def get_board(self) -> []:
+        return self.board
