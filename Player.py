db23e2bc43fbe2d2dfc94d5ae54e495d524ba7c2
hermessil <Silas.Hermes02@gmail.com>
2025-02-17 21:34:51 +0100

    setup method load_players in Player.py

--- Player.py
+++ Player.py
@@ -1,20 +1,28 @@
 class Player:
-    def __init__(self, name: str, deck: [], balance: int):
-        self.name: str = name
-        self.deck: [] = deck
-        self.balance: int = balance
+    def __init__(self, player_id: int, player_name: str, player_deck: list):
+        self.player_id: int = player_id
+        self.player_name: str = player_name
+        self.player_deck: list = player_deck
 
-    def set_name(self , name: str):
-        self.name: str = name
+    def has_bingo(self) -> bool:
+        """Checks whether player has bingo"""
+        """TODO: testing"""
+        # check rows
+        for row in self.player_deck:
+            if all(killer["used"] for killer in row):
+                return True
 
-    def get_name(self):
-        return self.name
+        # check columns
+        for col in range(len(self.player_deck[0])):
+            if all(row[col]["used"] for row in self.player_deck):
+                return True
 
-    def set_deck(self, deck: []):
-        self.deck: [] = deck
+        # main diagonal
+        if all(self.player_deck[i][i]["used"] for i in range(len(self.player_deck))):
+            return True
 
-    def get_deck(self):
-        return self.deck
+        # off diagonal
+        if all(self.player_deck[i][len(self.player_deck) - 1 - i]["used"] for i in range(len(self.player_deck))):
+            return True
 
-    def set_balance(self, balance: int):
-        self.balance: int = balance
+        return False
