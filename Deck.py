ac6d69cb68d8de226a93cb6e5cbaf1c46bdee401
hermessil <Silas.Hermes02@gmail.com>
2025-02-17 17:00:56 +0100

    add TODOs to Deck.py

--- Deck.py
+++ Deck.py
@@ -1,6 +1,14 @@
+import Player
+
 class Deck:
     def __init__(self, killer: []):
         self.killers: [] = killer
 
     def set_killers(self, killers: []):
         self.killers: [] = killers
+
+    def import_deck(self):
+        """TODO: method to import Bingo Deck from json file"""
+
+    def export_deck(self):
+        """TODO: method to import Bingo Deck from json file"""
