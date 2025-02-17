7a2cc1b93824ab7948c4fdb5849aed7e8ac92c33
hermessil <Silas.Hermes02@gmail.com>
2025-02-17 17:32:17 +0100

    setup method import_deck (not tested)

--- Deck.py
+++ Deck.py
@@ -8,13 +8,21 @@ class Deck:
     def set_killers(self, killers: []):
         self.killer_grid: [] = killers
 
-    def import_deck(self):
+    @staticmethod
+    def import_deck(json_data: str, bingo_id: int, player_id: int):
         """TODO: method to import Bingo Deck from json file"""
+        for bingo in json_data["bingos"]:
+            if bingo["bingo_id"] == bingo_id:
+                for player in bingo["players"]:
+                    killer_grid = [[entry["killer"] for entry in row] for row in player["deck"]]
+                    return Deck(killer_grid)
+        return None
 
+    @staticmethod
     def export_deck(self):
         """TODO: method to import Bingo Deck from json file"""
 
-    def has_duplicate_killers(self):
+    def has_duplicate_killers(self) -> bool:
         """Check whether deck has a killer more than once"""
         killer_set = set()
         for row in self.killer_grid:
