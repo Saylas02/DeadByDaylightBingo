a9e0025768b95bc8b117bb3e0bbfd99f4f947a24
hermessil <Silas.Hermes02@gmail.com>
2025-02-17 21:35:22 +0100

    setup method load_players in Game.py

--- Game.py
+++ Game.py
@@ -1,19 +1,17 @@
 import Deck
 import Player
+import json
 
 class Game:
-    def __init__(self, player: [], board: []):
-        self.player: [] = player
-        self.board: [] = board
+    def __init__(self, json_data: str, bingo_id: int):
+        """Loading bingo game and saves all Decks of players"""
+        self.bingo_id: int = bingo_id
+        self.players: list[Player] = []
 
-    def set_player(self, player: []):
-        self.player: [] = player
-
-    def get_player(self) -> []:
-        return self.player
-
-    def set_board(self, board: []):
-        self.board: [] = board
-
-    def get_board(self) -> []:
-        return self.board
+    def load_players(self, json_data: str):
+        """Create player object for all players of a bingo game"""
+        """TODO: test method"""
+        for bingo in json_data["bingos"]:
+            if bingo["bingo_id"] == self.bingo_id:
+                return [Player(player["player_id"], player["player_name"], player["player_deck"]) for player in bingo["players"]]
+            return []
